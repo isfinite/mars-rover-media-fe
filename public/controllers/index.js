a774551e905d3fe5e254fd86f635e83768220d24
Lauren Smith <isfinite@gmail.com>
2014-04-25 12:05:21 -0500

    creating socket conn

--- public/controllers/index.js
+++ public/controllers/index.js
@@ -1,12 +1,17 @@
 'use strict';
 
+var socket = io.connect('http://localhost:3000');
+
 angular
 	.module('mrmFE', [])
 	.controller('IndexController', ['$scope', '$http', function($scope, $http) {
 		$http.jsonp('http://localhost:3000/api/stats?callback=JSON_CALLBACK')
 			.success(function(data) {
-				$scope.totalImages = data.totalImages;
+				$scope.totalImages = data.totals.media.full + data.totals.media.thumbnail;
 			});
 
-		$scope.greet = 'test';
+		socket.on('stats', function(data) {
+			$scope.totalImages = data.totals.media.full + data.totals.media.thumbnail;
+			$scope.$digest();
+		});
 	}]);
